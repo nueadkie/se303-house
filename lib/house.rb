94ce93052223233bbdfdff19dde03d527d6986fa
Chris Miraflor <chris.j.miraflor@gmail.com>
2022-04-24 10:47:47 -0700

    Added invocation of phrase 11 method to default line

--- lib/house.rb
+++ lib/house.rb
@@ -42,7 +42,7 @@ class House
     when 10
       "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is #{animals_belong_to_phrase(n)}#{farmer_kept_phrase}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{animals_belong_to_phrase(n)}#{farmer_kept_phrase(n)}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 end
