d72828e84910601a38f48c3552a6bd8924f4ac35
Chris Miraflor <chris.j.miraflor@gmail.com>
2022-04-24 11:26:49 -0700

    Deleted case statement

--- lib/house.rb
+++ lib/house.rb
@@ -92,12 +92,6 @@ class House
   end
 
   def line(n)
-    case n
-    when 1
-      "This is #{animals_belong_to_phrase(n)}#{farmer_kept_phrase(n)}#{rooster_woke_phrase(n)}#{priest_married_phrase(n)}#{man_kissed_phrase(n)}#{maiden_milked_phrase(n)}#{cow_tossed_phrase(n)}#{dog_worried_phrase(n)}#{cat_killed_phrase(n)}#{rat_ate_phrase(n)}#{malt_lay_phrase(n)}the house that Jack built.\n"
-    else
-      "This is #{animals_belong_to_phrase(n)}#{farmer_kept_phrase(n)}#{rooster_woke_phrase(n)}#{priest_married_phrase(n)}#{man_kissed_phrase(n)}#{maiden_milked_phrase(n)}#{cow_tossed_phrase(n)}#{dog_worried_phrase(n)}#{cat_killed_phrase(n)}#{rat_ate_phrase(n)}#{malt_lay_phrase(n)}the house that Jack built.\n"
-    end
     "This is #{animals_belong_to_phrase(n)}#{farmer_kept_phrase(n)}#{rooster_woke_phrase(n)}#{priest_married_phrase(n)}#{man_kissed_phrase(n)}#{maiden_milked_phrase(n)}#{cow_tossed_phrase(n)}#{dog_worried_phrase(n)}#{cat_killed_phrase(n)}#{rat_ate_phrase(n)}#{malt_lay_phrase(n)}the house that Jack built.\n"
   end
 end
