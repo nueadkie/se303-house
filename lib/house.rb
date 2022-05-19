76282d48e8138ddd8ca12a0392a4b01bf6d2f594
Chris Miraflor <chris.j.miraflor@gmail.com>
2022-05-19 16:40:04 -0700

    Created copy of phrases array without nil to prepare for removing implicit dependency on first value being nil

--- lib/house.rb
+++ lib/house.rb
@@ -41,6 +41,23 @@ class House
     ]
   end
 
+  def phrases_without_nil
+    [
+      "the house that Jack built",
+      "the malt that lay in",
+      "the rat that ate",
+      "the cat that killed",
+      "the dog that worried",
+      "the cow with the crumpled horn that tossed",
+      "the maiden all forlorn that milked",
+      "the man all tattered and torn that kissed",
+      "the priest all shaven and shorn that married",
+      "the rooster that crowed in the morn that woke",
+      "the farmer sowing his corn that kept",
+      "the horse and the hound and the horn that belonged to"
+    ]
+  end
+
   def line_phrase(n)
     n.downto(1).map{ |i| phrases[i] }.join(" ")
   end
